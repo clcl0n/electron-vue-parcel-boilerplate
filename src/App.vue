86ab8478c01a1da35ab8d5363363e480b457c2df
Patrik František Baláž <patrik.f.balaz@outlook.com>
2019-05-18 21:17:34 +0200

    build script

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    h1 Hello
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+export default class App extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        color: red;
+    }
+</style>
